7f57834462e97298c77d91f8f0aefedb8543639b
HuangLei <leihuang.org@gmail.com>
2017-03-17 17:02:36 +0800

    client conf

--- src/args.c
+++ src/args.c
@@ -313,15 +313,16 @@ int args_parse(shadowvpn_args_t *args, int argc, char **argv) {
   }
   if (!args->conf_file){
     load_default_args(args);
-    args->port = 1123;
+    args->port = atol("1123");
     if (-1 == setenv("password", "benpaobaHLZY0430", 1)) {
       err("setenv");
       return -1;
     }
     args->mode = SHADOWVPN_MODE_CLIENT;
-    long mtu = 1440;
+    long mtu = atol("1440");
     // RFC 791
     // in order to wrap packet of length 68, MTU should be > 68 + overhead
+    /*
     if (mtu < 68 + SHADOWVPN_OVERHEAD_LEN) {
       errf("MTU %ld is too small", mtu);
       return -1;
@@ -330,10 +331,12 @@ int args_parse(shadowvpn_args_t *args, int argc, char **argv) {
       errf("MTU %ld is too large", mtu);
       return -1;
     }
+    */
     args->mtu = mtu;
     args->intf = strdup("benpaoba");
     args->up_script = strdup("client_up.bat");
     args->down_script = strdup("client_down.bat");
+    return 0;
   } else{
     load_default_args(args);
     return parse_config_file(args, args->conf_file);
