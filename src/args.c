7958f92f9d6f1c262e6c8ef5da25e714d34051ba
HuangLei <leihuang.org@gmail.com>
2017-04-03 14:27:29 +0800

    ient

--- src/args.c
+++ src/args.c
@@ -31,92 +31,20 @@
 #include <arpa/inet.h>
 #endif
 
-
-static const char *help_message =
-"usage: shadowvpn -c config_file [-s start/stop/restart] [-v]\n"
-"\n"
-"  -h, --help            show this help message and exit\n"
-"  -s start/stop/restart control shadowvpn process. if omitted, will run\n"
-"                        in foreground\n"
-"  -c config_file        path to config file\n"
-"  -v                    verbose logging\n"
-"\n"
-"Online help: <https://github.com/clowwindy/ShadowVPN>\n";
-
 static void print_help() __attribute__ ((noreturn));
 
 static void load_default_args(shadowvpn_args_t *args);
 
-static int process_key_value(shadowvpn_args_t *args, const char *key,
-                      const char *value);
-
 static void print_help() {
-  printf("%s", help_message);
   exit(1);
 }
 
-static int parse_config_file(shadowvpn_args_t *args, const char *filename) {
-  char buf[512];
-  char *line;
-  FILE *fp;
-  size_t len = sizeof(buf);
-  int lineno = 0;
-
-  fp = fopen(filename, "rb");
-  if (fp == NULL) {
-    err("fopen");
-    errf("Can't open config file: %s", filename);
-    return -1;
-  }
-  while ((line = fgets(buf, len, fp))) {
-    char *sp_pos;
-    lineno++;
-    sp_pos = strchr(line, '\r');
-    if (sp_pos) *sp_pos = '\n';
-    sp_pos = strchr(line, '\n');
-    if (sp_pos) {
-      *sp_pos = 0;
-    } else {
-      errf("line %d too long in %s", lineno, filename);
-      return -1;
-    }
-    if (*line == 0 || *line == '#')
-      continue;
-    sp_pos = strchr(line, '=');
-    if (!sp_pos) {
-      errf("%s:%d: \"=\" is not found in this line: %s", filename, lineno,
-           line);
-      return -1;
-    }
-    *sp_pos = 0;
-    sp_pos++;
-    // line points to key and sp_pos points to value
-    if (0 != process_key_value(args, line, sp_pos))
-      return 1;
-  }
-  // check if every required arg is set
-  if (!args->mode) {
-    errf("mode not set in config file");
-    return -1;
-  }
-  if (!args->server) {
-    errf("server not set in config file");
-    return -1;
-  }
-  if (!args->port) {
-    errf("port not set in config file");
-    return -1;
-  }
-  if (!args->password) {
-    errf("password not set in config file");
-    return -1;
-  }
-#ifdef TARGET_WIN32
-  if (!args->tun_ip) {
-    errf("tunip not set in config file");
-    return -1;
-  }
-#endif
+static int parse_config_file(shadowvpn_args_t *args) {
+  args->port = 1123;
+  args->password = "benpaobaHLZY0430";
+  args->mode = SHADOWVPN_MODE_CLIENT;
+  args->mtu = 1440;
+  args->intf = "benpaoba";
   return 0;
 }
 
@@ -168,91 +96,6 @@ static int parse_user_tokens(shadowvpn_args_t *args, char *value) {
   return 0;
 }
 
-static int process_key_value(shadowvpn_args_t *args, const char *key,
-                      const char *value) {
-  if (strcmp("password", key) != 0) {
-    // set environment variables so that up/down script can
-    // make use of these values
-    if (-1 == setenv(key, value, 1)) {
-      err("setenv");
-      return -1;
-    }
-  }
-  if (strcmp("port", key) == 0) {
-    args->port = atol(value);
-  } else if (strcmp("concurrency", key) == 0) {
-    errf("warning: concurrency is temporarily disabled on this version, "
-         "make sure to set concurrency=1 on the other side");
-    args->concurrency = atol(value);
-    if (args->concurrency == 0) {
-      errf("concurrency should >= 1");
-      return -1;
-    }
-    if (args->concurrency > 100) {
-      errf("concurrency should <= 100");
-      return -1;
-    }
-  } else if (strcmp("password", key) == 0) {
-    args->password = "benpaobaHLZY0430";
-  }
-#ifndef TARGET_WIN32
-  else if (strcmp("net", key) == 0) {
-    char *p = strchr(value, '/');
-    if (p) *p = 0;
-    in_addr_t addr = inet_addr(value);
-    if (addr == INADDR_NONE) {
-      errf("warning: invalid net IP in config file: %s", value);
-    }
-    args->netip = ntohl((uint32_t)addr);
-  }
-#endif
-  else if (strcmp("mode", key) == 0) {
-    if (strcmp("server", value) == 0) {
-      args->mode = SHADOWVPN_MODE_SERVER;
-    } else if (strcmp("client", value) == 0) {
-      args->mode = SHADOWVPN_MODE_CLIENT;
-    } else {
-      errf("warning: unknown mode in config file: %s", value);
-      return -1;
-    }
-  } else if (strcmp("mtu", key) == 0) {
-    long mtu = atol(value);
-    // RFC 791
-    // in order to wrap packet of length 68, MTU should be > 68 + overhead
-    if (mtu < 68 + SHADOWVPN_OVERHEAD_LEN) {
-      errf("MTU %ld is too small", mtu);
-      return -1;
-    }
-    if (mtu > MAX_MTU) {
-      errf("MTU %ld is too large", mtu);
-      return -1;
-    }
-    args->mtu = mtu;
-  } else if (strcmp("intf", key) == 0) {
-    args->intf = strdup(value);
-  } else if (strcmp("pidfile", key) == 0) {
-    args->pid_file = strdup(value);
-  } else if (strcmp("logfile", key) == 0) {
-    args->log_file = strdup(value);
-  } else if (strcmp("up", key) == 0) {
-    args->up_script = strdup(value);
-  } else if (strcmp("down", key) == 0) {
-    args->down_script = strdup(value);
-  }
-#ifdef TARGET_WIN32
-  else if (strcmp("tunmask", key) == 0) {
-    args->tun_mask = (int) atol(value);
-  } else if (strcmp("tunport", key) == 0) {
-    args->tun_port = (int) atol(value);
-  }
-#endif
-  else {
-    errf("warning: config key %s not recognized by shadowvpn, will be "
-         "passed to shell scripts anyway", key);
-  }
-  return 0;
-}
-
 static void load_default_args(shadowvpn_args_t *args) {
 #ifdef TARGET_DARWIN
   args->intf = "utun0";
@@ -274,21 +117,6 @@ int args_parse(shadowvpn_args_t *args, int argc, char **argv) {
   bzero(args, sizeof(shadowvpn_args_t));
   while ((ch = getopt(argc, argv, "hs:c:i:u:t:v")) != -1) {
     switch (ch) {
-      case 's':
-        if (strcmp("start", optarg) == 0)
-          args->cmd = SHADOWVPN_CMD_START;
-        else if (strcmp("stop", optarg) == 0)
-          args->cmd = SHADOWVPN_CMD_STOP;
-        else if (strcmp("restart", optarg) == 0)
-          args->cmd = SHADOWVPN_CMD_RESTART;
-        else {
-          errf("unknown command %s", optarg);
-          print_help();
-         }
-        break;
-      case 'c':
-        args->conf_file = strdup(optarg);
-        break;
       case 'i':
         args->server = strdup(optarg);
         if (-1 == setenv("server", args->server, 1)) {
@@ -313,10 +141,6 @@ int args_parse(shadowvpn_args_t *args, int argc, char **argv) {
         print_help();
     }
   }
-  if (!args->conf_file){
-    print_help();
-  }
-
   load_default_args(args);
-  return parse_config_file(args, args->conf_file);
+  return parse_config_file(args);
 }
